142569ba077113d3007535713062f1592e96cf72
jnthnexpdev <jnthnexpdev@gmail.com>
2026-02-20 17:05:00 -0600

    feat(contact): add contact section with responsive layout and form

---
+++ src/app/features/contact/contact.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LucideAngularModule } from 'lucide-angular';
+import { Phone, MapPinned, Mail, AtSignIcon, Building, User, MessagesSquare } from 'lucide-angular/src/icons';
+
+@Component({
+  selector: 'app-contact',
+  imports: [ TranslatePipe, LucideAngularModule ],
+  templateUrl: './contact.html',
+  styleUrl: './contact.css',
+})
+export class Contact {
+    readonly Phone = Phone;
+    readonly Mail = Mail;
+    readonly MapPinned = MapPinned;
+    readonly Building  = Building;
+    readonly User = User;
+    readonly AtSignIcon = AtSignIcon;
+    readonly MessagesSquare = MessagesSquare;
+}
