9c0c95aad96db182729ff3f0824c286aebd37d2c
jnthnexpdev <jnthnexpdev@gmail.com>
2026-02-23 13:59:21 -0600

    feat(navbar): add new navigation option and implement scroll animations

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,13 +5,33 @@ import { Experience } from "./features/experience/experience";
 import { Skills } from "./features/skills/skills";
 import { Footer } from "./core/layout/footer/footer";
 import { Contact } from './features/contact/contact';
+import { AnimateOnScrollDirective } from './shared/directives/animate-on-scroll.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Hero, Experience, Skills, Footer, Contact],
+  imports: [Navbar, Hero, Experience, Skills, Footer, Contact, AnimateOnScrollDirective],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Jnthnexpdev');
+    protected readonly title = signal('Jnthnexpdev');
+
+    ngAfterViewInit() {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if(entry.isIntersecting){
+                        entry.target.classList.add("in-view");
+                    }
+                })
+            },
+            { threshold : 0.2 }
+        );
+
+        const sections = document.querySelectorAll(".reveal");
+        sections.forEach((sec) => {
+            observer.observe(sec);
+        });
+    }
+    
 }
